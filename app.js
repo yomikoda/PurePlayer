9b5a1d49ffbc43b5bcc4b832386cc44529729adb
Yomikoda <yomikoda@gmail.com>
2016-11-25 13:25:04 +0100

    First Commit - Basic File structure

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use('/js', express.static('assets/js'));
+app.use('/css', express.static('assets/css'));
+app.use('/img', express.static('assets/img'));
+app.use('/fonts', express.static('assets/fonts'));
+
+app.get('/test', function(req, res){
+  res.send('<h1>Hello TEST</h1>');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/tpl/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
