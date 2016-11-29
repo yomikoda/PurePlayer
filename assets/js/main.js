152950e2ed1d6826c5e6428a562f2b44a69a88d4
Yomikoda <yomikoda@gmail.com>
2016-11-29 14:34:11 +0100

    Basis of Burger - Class Toggle - Fonts & Main.js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+   
+    /** BURGER CLICK Handling - Nav/logo/burger changes **/
+    $('#burger').on('click', function(){
+            $('#burger').toggleClass('rotated');
+    });
+    
+
+    
+    
+});
