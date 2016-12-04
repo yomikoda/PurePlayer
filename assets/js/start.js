ebf0c8b9932cdf5ec8df81cfec9303dd09e8ad4b
Yomikoda <yomikoda@gmail.com>
2016-12-04 22:37:48 +0100

    Beginning of Artists Page + Slight tweaks here and there to bg imgs

--- assets/js/start.js
+++ assets/js/start.js
@@ -3,19 +3,18 @@ $(function(){
 
  
     $( document ).ready(function() {
-        $('#biglogo').css({'top':'40%','transition':'all ease 2s','opacity':'1'});
+        $('#biglogo').css({'top':'30%','transition':'all ease 2s','opacity':'1'});
         $('#pitch p').css({'left':'0','transition':'all ease 2s','opacity':'1'});
      
          $('#pitch h2').animate({'left':'0'},2000,'linear',function(){
                   $('.section').each(function(i){
                     setTimeout(function(){
                     $('.section').eq(i).css('opacity','1');
-                    $('.section h3').eq(i).css({'bottom':'-60px','transition':'all ease 2s','opacity':'1'});
+                    $('.section h3').eq(i).css({'bottom':'-60px','transition':'all ease 1s','opacity':'1'});
                     },300 * (i+1));
                 });     
             });
-     
-         
+       
     });
    
 });
