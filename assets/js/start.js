7f37a460d75eda28c9931d7d9e797458c9992ac2
Yomikoda <yomikoda@gmail.com>
2016-12-04 22:03:43 +0100

    .each issue fixed on landing page

--- assets/js/start.js
+++ assets/js/start.js
@@ -6,14 +6,15 @@ $(function(){
         $('#biglogo').css({'top':'40%','transition':'all ease 2s','opacity':'1'});
         $('#pitch p').css({'left':'0','transition':'all ease 2s','opacity':'1'});
      
-         $('#pitch h2').animate({'left':'0'},3000,'linear',function(){
+         $('#pitch h2').animate({'left':'0'},2000,'linear',function(){
                   $('.section').each(function(i){
                     setTimeout(function(){
                     $('.section').eq(i).css('opacity','1');
-                    },500 * (i+1));
+                    $('.section h3').eq(i).css({'bottom':'-60px','transition':'all ease 2s','opacity':'1'});
+                    },300 * (i+1));
                 });     
             });
-        $('.section h3').css({'bottom':'-60px','transition':'all ease 1s','opacity':'1'});
+     
          
     });
    
