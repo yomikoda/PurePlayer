5cb92f73737a406f5262d274da8ee5b1625d3725
Yomikoda <yomikoda@gmail.com>
2016-12-15 14:28:49 +0100

    Still broken player

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,7 +62,7 @@ $(function(){
 
 var init = {
     
-    
+   
     index : function(){
         $('#biglogo').css({'top':'30%','transition':'all ease 1s','opacity':'1'});
         $('#pitch p').css({'left':'0','transition':'all ease 1s','opacity':'1'});
@@ -133,6 +133,50 @@ var init = {
     });
         
     },
+    
+     clickindex : function(){
+            $('.artists').on('click', function () {       
+        $('main').fadeOut(600, function () {
+            $('main').load('../../tpl/load/artists_load.html', init.artist)
+        });
+    });
+
+
+    $('.themes').click(function(){
+        $('main').fadeOut(600, function () {
+            $('main').load('../../tpl/load/themes_load.html', init.themes)
+        });
+    });
+
+    $('.genre').click(function(){
+        $('main').fadeOut(600, function () {
+            $('main').load('../../tpl/load/genre_load.html', init.artist)
+        });
+    });
+
+    $('.playlists').click(function(){
+        $('main').fadeOut(600, function () {
+            $('main').load('../../tpl/load/playlists_load.html', init.artist)
+        });
+    });
+
+
+    $('#logo_nav').click(function(){
+        $('main').fadeOut(600, function () {
+            $('main').load('../../tpl/load/mainindex_load.html', init.index)
+        });
+    });
+
+    // ARTIST PAGE LOADS
+
+    $('.artist').on('click', function () {       
+        $('.artist_profile').fadeOut(600, function () {
+            $('.artist_profile').load('../../tpl/load/artistprofile.html', init.artistprofile)
+        });
+});
+
+        
+    }
     
 }
 
